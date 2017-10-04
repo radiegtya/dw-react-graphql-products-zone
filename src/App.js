9f7514ce9ac1deb25a8baf8ad60b9ce488449065
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-10-04 16:41:22 +0700

    30. Fetch All

--- src/App.js
+++ src/App.js
@@ -6,16 +6,33 @@ import {graphql} from 'react-apollo';
 
 class App extends Component {
 
+  renderRow(category, index){
+    return (
+      <li key={index}>{category.name}</li>
+    )
+  }
+
   render() {
+    if(this.props.data.loading){
+      return <div>Loading...</div>
+    }
+
     return (
       <div className="App">
-
+        <ul>
+          {this.props.data.categories.map((category, index) => this.renderRow(category, index))}
+        </ul>
       </div>
     );
   }
 
 }
 
-export default graphql(
-  //query
-)(App);
+export default graphql(gql`
+  query allCategories{
+    categories{
+      id
+      name
+    }
+  }
+`)(App);
